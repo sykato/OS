6ff16cacd60c68b2697007e6322f85f4b94c6f2f
PIBO0120001 Терещенко Александр Юрьевич <PIBO0120001@sibupk.su>
2022-05-22 14:28:07 +0700

    Лабораторные ОС

---
+++ OS_Lab_1/OS_Lab_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace os_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(worker);
+            myThread.Start();
+
+            Console.Write("Размер массива: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Random random = new Random();
+            int[] mass = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                mass[i] = random.Next(10);
+                Console.WriteLine(mass[i]);
+            }
+        }
+
+        static void worker()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+    }
+}
